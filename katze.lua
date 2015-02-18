b6f836c2f84abd7fb62a030a6c29690c04f91d4e
etcher3rd <etcher3rd@users.noreply.github.com>
2015-02-18 07:38:01 +0100

    Update katze.lua

--- katze.lua
+++ katze.lua
@@ -1,7 +1,9 @@
-package.path  = package.path..";.\\LuaSocket\\?.lua"
-package.cpath = package.cpath..";.\\LuaSocket\\?.dll"
+package.path  = package.path..";./LuaSocket/?.lua"
+package.cpath = package.cpath..";./LuaSocket/?.dll"
 
 k = {}
+k.dir.main = lfs.writedir().."/Scripts"
+k.dir.logs = lfs.writedir().."/Logs/KatzePit"
 k.current_aircraft = nil
 k.config = {}
 k.sioc = {}
@@ -32,40 +34,7 @@ k.loop.fps_counter.tot = 0
 
 
 
--------------------------------------------------------------------------------
--- Logging & debug
-k.debug = true
-k.log_file = nil -- fichier log
-k.log = function (message)
-
-	-- Création du fichier de log des communication serveur, s'il n'existe pas
-	-- Format , KTZ-SIOC3000_ComLog-yyyymmdd-hhmm.csv
-	--
-	if k.debug then
-		
-		if not k.log_file then
-			-- création du fichier log si nécessaire
-       			k.log_file = io.open(lfs.writedir().."Logs\\KatzePit\\KTZ-SIOC5010_ComLog-"..os.date("%Y%m%d-%H%M")..".csv", "w")
-				
-			-- Ecriture de l'entète dans le fichier
-			if k.log_file then
-				k.log_file:write("*********************************************;\n")
-				k.log_file:write("*     Fichier Log des Communications SIOC   *;\n")
-				k.log_file:write("*     Par KaTZe  -  http://www.3rd-wing.net *;\n")
-				k.log_file:write("*     Version FC3  du 02/02/2015            *;\n")
-				k.log_file:write("*********************************************;\n\n")
-			end
-			
-		end
-	
-		-- Ecriture des données dans le fichier existant
-		if k.log_file then
-			k.log_file:write(string.format(" %s ; %s",os.clock(),message),"\n")
-		end
-	end
-end
-
-k.log("module de logging chargé")
+dofile(k.dir.main.."/logging.lua")
 
 
 -- k.log("remplacement de la fonction \"dofile\" par une version loggée")
