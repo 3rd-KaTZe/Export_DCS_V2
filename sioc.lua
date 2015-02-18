42d1f71dc1c2bb3309d6a6ac23c2be1544a27706
etcher3rd <etcher3rd@users.noreply.github.com>
2015-02-18 13:26:40 +0100

    Update sioc.lua

--- sioc.lua
+++ sioc.lua
@@ -1,20 +1,25 @@
 env.info("KTZ_PIT: chargement du module \"sioc.lua\"")
 
+env.info("KTZ_PIT: chargement des dépendandes pour le socket")
+package.path  = package.path..";./LuaSocket/?.lua"
+package.cpath = package.cpath..";./LuaSocket/?.dll"
+env.info("KTZ_PIT: chargement des dépendandes pour le socket réussi")
+
 k.sioc.ok = false -- "true" si le socket SIOC est connecté
 k.sioc.socket = require("socket") -- socket SIOC client
 k.sioc.buffer = {} -- tampon SIOC
 
 k.sioc.connect = function () -- fonction de connection à sioc
-	k.log("sioc_connect()")
+	k.info("connexion à SIOC")
 	-- on retombe sur les valeurs par défaut si on ne les trouve pas
 	host = k.config.sioc.ip or "127.0.0.1"
-    port = k.config.sioc.port or 8092
-	k.log("sioc_connect: ip:"..host.." port:"..port)
-	k.log("sioc_connect: ouverture du socket")
+	port = k.config.sioc.port or 8092
+	k.log("connexion à SIOC: ip:"..host.." port:"..port)
+	k.log("connexion à SIOC: ouverture du socket")
 	k.sioc.socket = socket.try(socket.connect(host, port)) -- connect to the listener socket
-	k.log("sioc_connect: socket.tcp-nodelay: true")
+	k.log("connexion à SIOC: socket.tcp-nodelay: true")
 	k.sioc.socket:setoption("tcp-nodelay",true) -- set immediate transmission mode
-	k.log("sioc_connect: socket.timeout: 0.1")
+	k.log("connexion à SIOC: socket.timeout: 0.1")
 	k.sioc.socket:settimeout(.01) -- set the timeout for reading the socket)
 ------------------------------------------------------------------------
 -- 	Offset de SIOC qui seront écoutés								  --
@@ -26,56 +31,53 @@ k.sioc.connect = function () -- fonction de connection à sioc
 	inputs [2]=2
 	local x, i
     	local s = ""
-	k.log("sioc_connect: création du handshake de SIOC")
+	k.log("connexion à SIOC: création du handshake de SIOC")
 	for x,i in pairs(inputs)
 	do
 	    s = s..x..":"
 	end
-	k.log("sioc_connect: handshake SIOC: "..s)
-	k.log("sioc_connect: envoi du handshake à SIOC")
+	k.log("connexion à SIOC: handshake SIOC: "..s)
+	k.log("connexion à SIOC: envoi du handshake à SIOC")
 	socket.try(k.sioc.socket:send("Arn.Inicio:"..s.."\n"))
 	k.sioc.contact = ("Arn.Inicio:"..s.."\n")
 	k.sioc.msg = "INIT-->;" .. k.sioc.contact
-	k.log("sioc_connect: contact: "..k.sioc.contact)
-	k.log("sioc_connect: msg: "..k.sioc.msg)
+	k.log("connexion à SIOC: contact: "..k.sioc.contact)
+	k.log("connexion à SIOC: msg: "..k.sioc.msg)
+	k.info("connexion à SIOC: réussie")
 	k.sioc.ok = true
 end
 
-k.sioc.send = function (strAttribut,valeur)
+k.sioc.send = function (attr_int, val_int)
 		-- Option possible : Décalage des exports vers une plage SIOC
 		-- Indiquer dans siocConfig.lua la plage désirée
 		-- newAtt = tonumber(strAttribut) + siocConfig.plageSioc
-		k.log("attrib: "..strAttribut)
-		k.log("valeur: "..valeur)
-		local strNew = tostring(strAttribut)
-		local strValeur = string.format("%d",valeur)
-		if (strValeur ~= k.sioc.buffer[strNew]) then
-			-- On stock la nouvelle valeur dans la table buffer
-			k.sioc.buffer[strNew] = strValeur ;
-			-- Envoi de la nouvelle valeur
-			socket.try(k.sioc.socket:send(string.format("Arn.Resp:%s=%.0f:\n",strNew,strValeur)))
-			local messageEnvoye = "OUT--> ;" .. (string.format("Arn.Resp:%s=%.0f:",strNew,strValeur))
-			-- Log du message envoyé
-			k.log(messageEnvoye)
+		local attr_str = tostring(attr_int)
+		local val_str = string.format("%d",val_int)
+		k.log("envoi SIOC: attrib: "..attr_str)
+		k.log("envoi SIOC: valeur: "..val_str)
+		if (val_str ~= k.sioc.buffer[attr_str]) then
+			k.sioc.buffer[attr_str] = val_str ;
+			local msg = string.format("Arn.Resp:%s=%.0f:\n", attr_str, val_str)
+			k.log("envoi SIOC: msg à envoyer: "..msg)
+			socket.try(k.sioc.socket:send(msg))
+			k.log("envoi SIOC: msg envoyé")
 		end		
 end
 
 k.sioc.receive = function ()
-	k.log("sioc.receive()")
-	-- Check for data/string from the SIOC server on the socket
-    --logCom("*** Fonction recupInfo activated","\n")
-	-- socket.try(c:send("Arn.Resp"))
-	local messageRecu = k.sioc.socket:receive()
-	if messageRecu then
-		local messagelog = "IN-->;".. tostring(messageRecu)
-		--logCom(messagelog)
-		local s,l,typeMessage = string.find(messageRecu,"(Arn.%w+)");
+	k.log("réception SIOC")
+	local msg_rec = k.sioc.socket:receive()
+	if msg_rec then
+		k.log("réception SIOC: message reçu: "..tostring(msg_rec))
+		local s,l,typeMessage = string.find(msg_rec,"(Arn.%w+)");
 		typeMessage = tostring(typeMessage);
 		------------------------------------------------------------
 		-- Les types de message acceptés :                        --
 		--                                                        --
 		-- Arn.Vivo   : Le serveur à reçu "Arn.Vivo": du client   -- 
 		--              Le serveur répond "Arn.Vivo"              --
+		--				Il s'agit de l'équivalent SIOC d'un		  --
+		--				ping-pong.								  --
 		--														  --
 		-- Arn.Resp   : Message pour l'execution des commandes    --
 		--              a noter que Arn.Resp:1=0: remets le       --
@@ -83,37 +85,32 @@ k.sioc.receive = function ()
 		--				commande exécuté                          --
 		------------------------------------------------------------
 		if typeMessage == "Arn.Resp" then
------------------------------------------------------------------------------
--- 	Lecture du message												  --*************************************************************
-----------------------------------------------------------------------------
-			-- (message type par exemple :1=3:0=23:6=3456)
-			local debut,fin,message = string.find(messageRecu,"([%d:=-]+)")
-			-- logCom(message)
-			-- longueur du message
-			local longueur
-			longueur = fin - debut
-			--logCom(longueur)
-			-- découpe du message en commande et envoi à DCS
-			local commande,Schan,chan,Svaleur,valeur,i,a,b,c,d,e,f,lim,device,bouton,typbouton,val
-			lim = 0
-			while lim < longueur do
-				a,b,commande = string.find(message,"([%d=-]+)", lim)
-				--logCom(commande)
-				c,d,Schan = string.find(commande, "([%d-]+)")
-				chan = tonumber(Schan)
+			k.log("réception SIOC: lecture du message")
+			local s, e, msg = string.find(msg,"([%d:=-]+)")
+			k.log("réception SIOC: message décomposé: "..msg)
+			local l = e - s -- longueur totale du message
+			k.log("réception SIOC: longueur du message: "..l)
+			local x = 0 -- index de lecture (seek pointer)
+			local cmd -- commande
+			local chan_str, chan_int -- canal SIOC
+			local val_str,val_int -- valeur envoyée par SIOC
+			local b,d -- pointeurs de position dans la chaîne de caractères intermédiaire
+			while x < l do
+				_,b,cmd = string.find(msg,"([%d=-]+)", l)
+				k.log("réception SIOC: commande: "..cmd)
+				k.log("réception SIOC: longueur du morceau: "..b)
+				_,d,chan_str = string.find(cmd, "([%d-]+)")
+				chan_int = tonumber(chan_str)
 				--logCom(string.format(" Offset = %.0f",chan,"\n"))
-				e,f,Svaleur = string.find(commande, "([%d-]+)",d+1)
-				valeur = tonumber(Svaleur)
-				--logCom(string.format(" Valeur = %.0f",valeur,"\n"))
------------------------------------------------------------------------------
--- 	Interpretation des commandes										  --*************************************************************
-----------------------------------------------------------------------------
+				_,_,val_str = string.find(cmd, "([%d-]+)",d+1)
+				val_int = tonumber(val_str)
 				-----------------------------------------------------------------
-				-- Canal #1 : Commande type FC2
+				-- Canal #1 : commande type FC2
 				-----------------------------------------------------------------
-				if chan ==1 and valeur > 0 then
+				if chan_int == 1 and val_int ~= 0 then
 					-- Envoi à LockOn, commande type Classique FC3
-					LoSetCommand(valeur)
+					k.log("réception SIOC: canal1, envoi de la valeur: "..val_str)
+					LoSetCommand(val_int)
 				end
 				-----------------------------------------------------------------
 				-- Canal #2 : Commande type DCS
@@ -124,80 +121,93 @@ k.sioc.receive = function ()
 				-- BBB = numero du bouton
 				-- P = Pas du rotateur
 				-- V = Valeur recu
-				if chan ==2 and valeur > 0 then
-					typbouton = tonumber(string.sub(Svaleur,1,1))
-					device = tonumber(string.sub(Svaleur,2,3))
-					bouton = tonumber(string.sub(Svaleur,4,6))
-					pas = tonumber(string.sub(Svaleur,7,7))
-					val = tonumber(string.sub(Svaleur,8,8))
-					--logCom(string.format(" Device = %.0f",device,"\n"))
-					--logCom(string.format(" Bouton = %.0f",bouton,"\n"))
-					--logCom(string.format(" Type = %.0f",typbouton,"\n"))
-					--logCom(string.format(" Valeur = %.0f",val,"\n"))
+				if chan_int ==2 and val_int > 0 then
+					k.log("réception SIOC: canal2")
+					bouton_type = tonumber(string.sub(val_str,1,1))
+					device = tonumber(string.sub(val_str,2,3))
+					bouton = tonumber(string.sub(val_str,4,6))
+					pas = tonumber(string.sub(val_str,7,7))
+					val = tonumber(string.sub(val_str,8,8))
+					k.log(string.format("réception SIOC: canal2: device = %.0f",device,"\n"))
+					k.log(string.format("réception SIOC: canal2: bouton = %.0f",bouton,"\n"))
+					k.log(string.format("réception SIOC: canal2: type = %.0f",bouton_type,"\n"))
+					k.log(string.format("réception SIOC: canal2: valeur = %.0f",val,"\n"))
 					-----------------------------------------------------------------
 					-- Type 1 : Simple On/Off
-					if typbouton == 1 then
+					if bouton_type == 1 then
 						-- Type interrupteur deux voies
 						-- Envoi à LockOn, commande Device, Bouton + 3000 , Argument
+						k.log("réception SIOC: canal2: bouton_type1")
 						GetDevice(device):performClickableAction(3000+bouton,val)
 					end
 					-----------------------------------------------------------------
 					-- Type 2 : Simple On/Off avec Capot sur KA50 ... inutilisé sur Mi-8 ou UH-1
 					-- La séquence capot/bouton/capot est créé en 3 commandes successives par javascript
-					if typbouton == 2 then
+					if bouton_type == 2 then
 						-- Type interrupteur deux voies, avec capot , val = val * 1000
 						-- On ouvre, bascule, ferme
+						k.log("réception SIOC: canal2: bouton_type2")
 						GetDevice(device):performClickableAction(3000+bouton+1,val)
 						GetDevice(device):performClickableAction(3000+bouton,val)
 						GetDevice(device):performClickableAction(3000+bouton+1,val)
 					end
 					-----------------------------------------------------------------
 					-- Type 3 : 3 positions Bas/Mid/Haut
-					if typbouton == 3 then
+					if bouton_type == 3 then
 						-- Type interrupteur 3 positions  -1 , 0 , +1
 						-- On décale de -1 i.e. 0>>-1 , 1>>0 , 2>>1
+						k.log("réception SIOC: canal2: bouton_type3")
 						GetDevice(device):performClickableAction(3000+bouton,(val-1))
 					end
 					-----------------------------------------------------------------
 					-- Type 4 : Rotateur Multiple (Décimal) ... 
-					if typbouton == 4 then
+					if bouton_type == 4 then
 						-- Type interrupteur rotary , 0.0 , 0.1 , 0.2 , 0.3 , ...
 						-- On envoie des valeur de 0 à X
+						k.log("réception SIOC: canal2: bouton_type4")
 						if pas < 2 then  -- Pas à 0 ou 1, incrément par 0.1
+							k.log("réception SIOC: canal2: bouton_type4: pas < 2")
 							GetDevice(device):performClickableAction(3000+bouton,val/10)
-						end
-						if pas == 2 then -- Pas à 2 , incrément par 0.05
+						elseif pas == 2 then -- Pas à 2 , incrément par 0.05
+							k.log("réception SIOC: canal2: bouton_type4: pas == 2")
 							GetDevice(device):performClickableAction(3000+bouton,val/20)
 						end
 					end
 					-----------------------------------------------------------------
 					-- Type 5 : Press Bouton ... commande suivie de mise à zero
-					if typbouton == 5 then
+					if bouton_type == 5 then
 						-- Type interrupteur press bouton
 						-- On envoie 1 puis zero
+						k.log("réception SIOC: canal2: bouton_type5")
 						GetDevice(device):performClickableAction(3000+bouton,val)
 						GetDevice(device):performClickableAction(3000+bouton,val*0)
 					end
 					-----------------------------------------------------------------
 					-- Type 6 : Rotateur Multiple (Décimal , Centré sur zero)
-					if typbouton == 6 then
+					if bouton_type == 6 then
 						-- Rotateur centré sur 0 , pas de 0.1, décalage négatif de "pas"/10
 						-- exemple si pas = 5 , alors 0 --> -0.5 , 1 --> -0.4 , ... 5 --> 0 , 9 --> 0.4
+						k.log("réception SIOC: canal2: bouton_type6")
 						GetDevice(device):performClickableAction(3000+bouton,val/10 - pas/10)
 					end
 					-----------------------------------------------------------------
 					-- Type 7 : Rotateur Multiple (Centésimal)
-					if typbouton == 7 then
+					if bouton_type == 7 then
 						-- Rotateur centesimal , incrément = ( 10 * pas + val )/ 100
+						k.log("réception SIOC: canal2: bouton_type7")
 						GetDevice(device):performClickableAction(3000+bouton,(pas * 10 + val)/100)
 					end
 				end
-				lim = b + 1
+			
+				x = b + 1 -- calage du pointeur sur la prochaine partie du message
+				k.log("réception SIOC: nouveau pointeur: "..x)
 			end
 		else
-			k.log("---Log: SIOC Message Incorrect ; non type Arn.Resp ; Message Ignoré -----", "\n")
+			k.log("réception SIOC: message autre que \"Arn.Resp\"; message ignoré")
 		end
-    end
+	else
+		k.info("réception SIOC: erreur lors de la lecture du socket")
+	end
 end
 
 env.info("KTZ_PIT: chargement du module \"sioc.lua\" réussi")
